6478b51572bb8750313d82981a6a83599b4030cd
Ben Shurts <bshurts93@gmail.com>
2019-02-15 16:03:48 -0600

    Added restart functionality and message box on win/loss. Refactored parts of the logic into smaller functions to clean up code. Small bug that incrementally increases the value of each click when restarting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,55 +15,77 @@ var userScoreDOM = $("#user-score");
 var targetScoreDOM = $("#target-score");
 var winsDOM = $("#wins");
 var losssesDOM = $("#losses");
-var redDOM = $("btn-red");
-var greenDOM = $("btn-green");
-
+var messageBox = $(".message-box");
+var messageText = $(".message");
 
 // ------------------------- //
 // FUNCTION DECLARATIONS
 // ------------------------- //
-var randNumScore = function () {
+function randNumScore() {
     return Math.floor(Math.random() * 15 + 16); // Gets a random number between 14 and 30
 }
-
-var randNumCrystal = function () {
+function randNumCrystal() {
     return Math.floor(Math.random() * 5 + 2); // Gets a random number between 2 and 6
 }
 
-var click = function (id, val) {
+function winOrLoss(result) {
+    if (result === "win") {
+        wins++;
+        messageText.text("You win!");
+        messageBox.addClass("message-box-active");
+        gameInit();
+    } else if (result === "loss") {
+        losses++
+        messageText.html("You lose!");
+        messageBox.addClass("message-box-active");
+        gameInit();
+    }
+}
+
+function click(id, val) {
     $(id).on("click", function () {
         currentScore += val;
         userScoreDOM.text(currentScore);
 
+        console.log("ID: " + id + ". Val: " + val + ".");
+
         if (currentScore == targetScore) {
-            console.log("you win");
-            $(".user-score").text("You win!");
-            gameInit();
+            winOrLoss("win");
         } else if (currentScore > targetScore) {
-            console.log("you lose");
-            $(".user-score").text("You lose!");
+            winOrLoss("loss");
         }
     });
 }
-
-var gameInit = function () {
-    // Put code here that sets up the game from the start
-
-    // Reset user score
+function resetUserScore() {
+    userScoreDOM.text("0");
+    console.log("score is reset");
     currentScore = 0;
-    // Assign a random number to the target score and push to DOM
-    targetScore = (randNumScore());  // Scoring value
-    targetScoreDOM.text(targetScore);  // Pass that value to the DOM
-
-    // Assign a unique random number to each crystal value
-    redVal = randNumCrystal();
-    greenVal = randNumCrystal();
-
-    // Run function for button click events
-    click("#btn-green", greenVal);
-    click("#btn-red", redVal);
 }
 
-// ------------------------- //
-// GAME PROCESS
-// ------------------------- //
+function gameInit() {
+    // Put code here that sets up the game from the start
+    document.onkeydown = function (e) {
+        keypress = e.key;
+
+        // Only run when enter is hit
+        if (keypress == "Enter") {
+            // Assign a random number to the target score and push to DOM
+            targetScore = (randNumScore());  // Scoring value
+            targetScoreDOM.text(targetScore);  // Pass that value to the DOM
+
+            // Assign a unique random number to each crystal value
+            redVal = randNumCrystal();
+            greenVal = randNumCrystal();
+
+            // Run function for button click events
+            click("#btn-green", greenVal);
+            click("#btn-red", redVal);
+
+            // Reset user score
+            resetUserScore();
+        }
+    };
+}
+
+
+gameInit();
